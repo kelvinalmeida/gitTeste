8c7f9b2d9e20c72584d942bcc1f88b072776dc39
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-10-14 14:59:13 -0300

    add console.log

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
- O volume na unidade C n∆o tem nome.
- O N£mero de SÇrie do Volume Ç E4B9-82D2
-
- Pasta de C:\Users\FABRI\OneDrive\Documentos\GitHub\gitTeste
-
-13/10/2023  19:58    <DIR>          .
-13/10/2023  19:58    <DIR>          ..
-13/10/2023  19:58                 0 index.js
-               1 arquivo(s)              0 bytes
-               2 pasta(s)   96.788.094.976 bytes dispon°veis
+console.log('hello')
